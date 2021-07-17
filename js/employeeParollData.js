e42cfe3131ee02e50a78f99aa5615f80ddef89b9
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-17 21:40:58 +0530

    added: welcome to add employee payroll

---
+++ js/employeeParollData.js
@@ -0,0 +1,13 @@
+class EmployeePayrollData {
+
+    constructor(...params) {
+        this.name = params[0]
+        this.profile = params[1];
+        this.gender = params[2];
+        this.department = params[3];
+        this.salary = params[4];
+        this.startDate = params[5];
+        this.notes = params[6];
+    }
+
+}
