37efd620337394eca67a322356d8618b049da082
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-18 11:58:05 +0530

    added: modify employee payroll class with new attributes and getters and setters

--- js/employeeParollData.js
+++ js/employeeParollData.js
@@ -1,13 +1,70 @@
 class EmployeePayrollData {
 
-    constructor(...params) {
-        this.name = params[0]
-        this.profile = params[1];
-        this.gender = params[2];
-        this.department = params[3];
-        this.salary = params[4];
-        this.startDate = params[5];
-        this.notes = params[6];
+    get id(){
+        return this._id;
     }
+
+    set id(id){
+        this._id = id;
+    }
+
+    get name(){
+        return this.name;
+    }
+
+    set name(name){
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-z\\s]{2,}$');
+        if (nameRegex.test(name))
+            this.name = name;
+        else throw 'Name is Incorrect'
+    }
+
+    get profilePic(){
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic){
+        this._profilePic = profilePic;
+    }
+
+    get gender(){
+        return this._gender;
+    }
+
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get department(){
+        return this._department;
+    }
+
+    set department(department){
+        this._department = department;
+    }
+
+    get salary(){
+        return this._salary;
+    }
+
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get notes(){
+        return this._notes;
+    }
+
+    set notes(notes){
+        this._notes = notes;
+    }
+
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name='" + this.name + "gender=" + this.gender + ", profilePic=" + this.profilePic + ", department=" + this.department + ", salary=" + this.salary + ", " + ", startDate=" + empDate + ", notes=" + this.notes;
+    }
+
 
 }
